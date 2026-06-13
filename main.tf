ce7a56a6df2424e41e06109932b9d29132d5dce6
Todd Oneil <onei0240@algonquinlive.com>
2026-06-13 18:07:29 -0400

    extracted subnet out of the vm and made it it's own resource. created and verified Network Interface Card

--- main.tf
+++ main.tf
@@ -36,11 +36,15 @@ resource "azurerm_virtual_network" "VNet" {
   name = "${var.labelPrefix}-vnet"
   location = azurerm_resource_group.resourceGroup.location
   address_space = [ "10.0.0.0/16" ]
-  subnet = [ {
-    name = "${var.labelPrefix}-subnet"
-    address_prefix = "10.0.1.0/24"
-} ]
   depends_on = [ azurerm_resource_group.resourceGroup ]
+}
+
+resource "azurerm_subnet" "main" {
+  name = "${var.labelPrefix}-subnet"
+  virtual_network_name = azurerm_virtual_network.VNet.name
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+  address_prefixes = [ "10.0.1.0/24" ]
+  depends_on = [ azurerm_virtual_network.VNet ]
 }
 
 resource "azurerm_public_ip" "pip" {
@@ -48,7 +52,7 @@ resource "azurerm_public_ip" "pip" {
   resource_group_name = azurerm_resource_group.resourceGroup.name
   location            = azurerm_resource_group.resourceGroup.location
   allocation_method   = "Dynamic"
-  depends_on = [ azurerm_virtual_network.VNet ]
+  depends_on = [ azurerm_subnet.main ]
 }
 
 # For production it is not recommended to allow "*" for source_address_prefix
@@ -81,4 +85,17 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
     }
    ]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name = "${var.labelPrefix}-nic"
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+  location = azurerm_resource_group.resourceGroup.location
+  
+  ip_configuration {
+    name = "primary"
+    subnet_id = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
 }
