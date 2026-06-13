41689ac52b20bfa1e3053aa42fe21c0f2457083c
Todd Oneil <onei0240@algonquinlive.com>
2026-06-13 17:02:39 -0400

    base template written and validated

---
+++ main.tf
@@ -0,0 +1,26 @@
+# configure terraform runtime requirements
+terraform {
+    required_version = ">= 1.1.0"
+
+    required_providers {
+      # Azure Resource Manager Provider and Version
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 3.0.2"
+      }
+      cloudinit = {
+        source = "hashicorp/cloudinit"
+        version = "2.3.3"
+      }
+    }
+}
+
+# Define providers and their configurations
+provider "azurerm" {
+    # Leave the features block empty to accept all defaults
+  features {}
+}
+
+provider "cloudinit" {
+    # configuration options
+}
